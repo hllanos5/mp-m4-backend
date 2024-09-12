20958eb1a70a93035c5a3fe1749b69943c25de91
Hans Llanos <hans.llanos@gmail.com>
2024-09-12 18:48:49 -0500

    implementacion de obtener imagen mediante url

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import { PORT } from './config/config.js'
-import userRoutes from './routes/user.routes.js'
-import authRoutes from './routes/auth.routes.js'
 import morgan from 'morgan'
 import { validateCORS } from './middlewares/middleware.js'
+import userRoutes from './routes/user.routes.js'
+import authRoutes from './routes/auth.routes.js'
+import imagesRoutes from './routes/images.routes.js'
+
 
 const app = express()
 app.use(morgan('dev'))
@@ -12,5 +14,6 @@ app.use(validateCORS)
 
 app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
+app.use('/api/images', imagesRoutes)
 
 app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`))
