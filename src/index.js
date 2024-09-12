19bff43ff5cc2738801d10fdec3a4cbcae6c153a
Hans Llanos <hans.llanos@gmail.com>
2024-09-12 15:36:38 -0500

    implementacion de listar usuario

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import { PORT } from './config/config.js'
+import userRoutes from './routes/user.routes.js'
 import morgan from 'morgan'
 
 const app = express()
 app.use(morgan('dev'))
 
+app.use('/api/user', userRoutes)
 
 app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`))
